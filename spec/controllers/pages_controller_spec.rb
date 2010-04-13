9ad376a60a41731940721f5fde4318346f04c007
slackerlord <slackerlord@msn.com>
2010-04-13 13:13:28 +0100

    Added static pages

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+	
+	it "should have the right title" do
+	  get 'home'
+	  response.should have_tag("title","Rails Tutorial App | Home")
+	end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+	
+	it "should have the right title" do
+	  get 'contact'
+	  response.should have_tag("title","Rails Tutorial App | Contact")
+	end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+	  get 'about'
+	  response.should be_success
+	end
+	
+	it "should have the right title" do
+	  get 'about'
+	  response.should have_tag("title","Rails Tutorial App | About")
+	end
+  end
+end
